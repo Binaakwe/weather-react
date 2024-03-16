675f5b113f10bf0f73b9998fb89c654181e98d38
Binaakwe <143651843+Binaakwe@users.noreply.github.com>
2024-03-16 16:29:30 -0400

    added weather project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+
+export default function App(props) {
+  let [weather, setWeather] = useState(" ");
+  
+  
+function showWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+    });
+  }
+
+ 
+    let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showWeather);
+  
+  
+  // function cityInquiry(event) {
+  //   setCity(event.target.value);
+  // }
+  let form = (
+    <form className="search-form">
+      <input
+        type="search"
+        placeholder="Enter a City..."
+        // onChange={cityInquiry}
+        className="search-input"
+      />
+      <input type="submit" value="Search" className="search-button" />
+    </form>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather-app">
+      <header>{form}</header>
+      <main>
+        <div className="current-weather">
+          <div>
+            <h1 className="city">{props.city}</h1>
+            <h3 className="weather-description">
+              {" "}
+              <div id="current-time">Thursday, 15:00</div>
+              <div className="description">{weather.description}</div>
+            </h3>
+            <div className="current-weather-details">
+              <p>
+                Humidity:{" "}
+                <span className="current-humidity">
+                  {Math.round(weather.humidity)} %
+                </span>
+                <br />
+                Wind Speed:{" "}
+                <span className="current-windspeed">
+                  {Math.round(weather.wind)}km/h
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="temperature-container">
+            <div className="current-temperature">
+              {Math.round(weather.temperature)}
+            </div>
+            <div className="temperature-unit">°C</div>
+            <div>
+              {" "}
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+                alt={weather.description}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
-
-export default App;
